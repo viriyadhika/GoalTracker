d49953c901dc7d555515e4a529dbbded5630a63e
virzzz <viriya.dhika0@gmail.com>
2020-07-03 23:51:13 +0800

    fixed date parsing bug in add record, implemented StatisticsActivity

---
+++ app/src/main/java/com/example/goaltracker/StatisticsActivity.java
@@ -0,0 +1,13 @@
+package com.example.goaltracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StatisticsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistics);
+    }
+}
